7b3774d99eb6e5b19e1b0b0ee595cfbca646898c
aizriel1123 <aizriel1123@gmail.com>
2022-11-28 12:14:27 +0800

    Initial commit

---
+++ exercise #4.py
@@ -0,0 +1,4 @@
+seconds = 365*24*60**2
+lightSpeed = 3*10**8
+lightYear = float(lightSpeed*seconds)
+print("Light travels", lightYear, "meters in a year.")
---
+++ exercise#3.py
@@ -0,0 +1,4 @@
+minutes = 60
+hours = 24
+days = 365
+print("The number of minutes in a year is", minutes*hours*days)
